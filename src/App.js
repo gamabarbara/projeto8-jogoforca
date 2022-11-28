9579b33e39655d54cfd9428dab2be46ad13d203e
gamabarbara <barbara.cp@outlook.com>
2022-11-28 15:38:07 -0300

    Projeto finalizado sem bônus

--- src/App.js
+++ src/App.js
@@ -43,42 +43,114 @@ function App() {
   const [erros, setErros] = useState(0);
   const [palavraEscolhida, setPalavraEscolhida] = useState([]);
   const [palavra, setPalavra] = useState([]);
+  const [letrasUsadas, setLetrasUsadas] = useState(alfabeto);
+  const [stringSemAcentos, setStringSemAcentos] = useState("");
+  const [chute, setChute] = useState("");
+  const [corDaPalavra, setCorDaPalavra] = useState("preto");
+  const [dataAnswer, setDataAnswer] = useState("");
 
   function iniciarJogo() {
     setDesabilitarInput(false);
+    setLetrasUsadas([]);
     sortearPalavra();
+    setErros(0);
+    setChute("");
+    setCorDaPalavra("preto");
+    setDataAnswer(stringSemAcentos);
+    console.log("dataAnswer", stringSemAcentos);
   }
 
+  function finalizarJogo() {
+    setLetrasUsadas(alfabeto);
+    setDesabilitarInput(true);
+    setChute("");
+    setPalavra(palavraEscolhida)
+  }
   function sortearPalavra() {
     const indice = Math.floor(Math.random() * palavras.length);
     const palavra = palavras[indice];
     const arrayPalavra = palavra.split("");
     setPalavraEscolhida(arrayPalavra);
     let traco = [];
-    arrayPalavra.forEach((letra) => traco.push(" _"))
+    arrayPalavra.forEach((letra) => traco.push(" _"));
     setPalavra(traco);
+    const palavraSemAcento = palavra
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    setStringSemAcentos(palavraSemAcento);
+  }
 
+  function clicouLetra(l) {
+    setLetrasUsadas([...letrasUsadas, l]);
+    if (stringSemAcentos.includes(l)) {
+      acertouLetra(l);
+    } else {
+      errouLetra(l);
+    }
+  }
+
+  function acertouLetra(l) {
+    let novaPalavra = [...palavra];
+    palavraEscolhida.forEach((letra, i) => {
+      if (stringSemAcentos[i] === l) {
+        novaPalavra[i] = letra;
+      }
+    });
+    setPalavra(novaPalavra);
+    if(!novaPalavra.includes(" _")) {
+      setCorDaPalavra("verde");
+      finalizarJogo();
+    }
+  }
+  function errouLetra() {
+    const novaQuantidadeErros = erros + 1;
+    setErros(novaQuantidadeErros);
+    if(novaQuantidadeErros === 6){
+      setCorDaPalavra("vermelho")
+      finalizarJogo();
+    }
+  }
+
+  function chutarPalavra() {
+    let stringEscolhida = palavraEscolhida.join("");
+    if (chute === stringEscolhida) {
+      setCorDaPalavra("verde");
+    } else {
+      setErros(6);
+      setCorDaPalavra("vermelho");
+    }
+    finalizarJogo();
   }
 
   return (
     <div className="container-tela">
       <div className="container-forca">
-        <img src={imagens[erros]} alt="imagem da forca" />
-        <button onClick={iniciarJogo}>Escolher palavra</button>
-        <h1>{palavra}</h1>
+        <img src={imagens[erros]} alt="imagem da forca" data-test="game-image"/>
+        <button onClick={iniciarJogo} data-test="choose-word">Escolher palavra</button>
+        <h1 className={corDaPalavra} data-test="word" data-answer={dataAnswer}>{palavra}</h1>
       </div>
 
       <div className="container-letras">
         {alfabeto.map((letra) => (
-          <button key={letra}>
+          <button
+            key={letra}
+            onClick={() => clicouLetra(letra)}
+            disabled={letrasUsadas.includes(letra)}
+            data-test="letter"
+          >
             {letra.toUpperCase()}
           </button>
         ))}
       </div>
       <div className="container-input">
         <span>Já sei a palavra!</span>
-        <input disabled={desabilitarInput} />
-        <button>Chutar</button>
+        <input
+          disabled={desabilitarInput}
+          value={chute}
+          onChange={(e) => setChute(e.target.value)}
+          data-test="guess-input"
+        />
+        <button onClick={chutarPalavra} data-test="guess-button">Chutar</button>
       </div>
     </div>
   );
